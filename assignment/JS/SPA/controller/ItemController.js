14abda36fd309ce6cf2d74f8d2d485d6124c7f09
ravindusadis <ravindusadis01@gmail.com>
2022-10-13 13:26:14 -0700

    get item values and assign variables

--- assignment/JS/SPA/controller/ItemController.js
+++ assignment/JS/SPA/controller/ItemController.js
@@ -1,3 +1,9 @@
 $("#saveItem").click(function (){
 
+    let itemCode = $("#txtItemCode").val();
+    let itemName = $("#txtItemName").val();
+    let qty = $("#txtItemQty").val();
+    let unitPrice = $("#txtItemUnitPrice").val();
+    let total = $("#txtItemTotal").val();
+
 });
