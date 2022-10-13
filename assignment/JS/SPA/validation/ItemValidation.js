81431eb153312de68496bafc729fe2020fd53dae
ravindusadis <ravindusadis01@gmail.com>
2022-10-13 15:05:16 -0700

    C update

--- assignment/JS/SPA/validation/ItemValidation.js
+++ assignment/JS/SPA/validation/ItemValidation.js
@@ -68,3 +68,17 @@ $("#txtItemUnitPrice").on('keydown', function (event) {
         }
     }
 });
+
+
+    function check(regex, txtField) {
+    let inputValue = txtField.val();
+    return regex.test(inputValue) ? true : false;
+}
+    function setTextError(txtField,error) {
+        if (txtField.val().length <= 0) {
+            defaultText(txtField, "");
+        } else {
+            txtField.css('border', '2px solid red');
+            txtField.parent().children('span').text(error);
+        }
+    }
