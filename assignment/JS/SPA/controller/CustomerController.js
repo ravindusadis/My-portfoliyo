6d2aa48ab700c935e0aff7e88995eba82e1d5593
ravindusadis <ravindusadis01@gmail.com>
2022-10-13 11:31:33 -0700

    set edits

--- assignment/JS/SPA/controller/CustomerController.js
+++ assignment/JS/SPA/controller/CustomerController.js
@@ -28,16 +28,21 @@ var customerObject = {
     contact : customerContact,
     salary : customerSalary
 }
+
+$("#updateCustomer").click(function (){
+
+});
 function loadAllCustomers() {
     $("#tblCustomer").empty();
     for (var customer of customers) {
 
-        var all = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td><td class="odd" style="width: 190px">
-                            <button class="btn btn-warning btn-mini" data-bs-target="#editCustomers"
-                                    data-bs-toggle="modal"><i class="fa-solid fa-pen-to-square"></i> Edit
-                            </button>
-                            <button class="btn btn-danger btn-mini"><i class="fa-solid fa-trash"></i> Delete</button>
-                        </td></tr>`;
+        var all = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td>
+                        <td><button class="btn btn-warning btn-mini" data-bs-target="#editCustomers"
+                        data-bs-toggle="modal" id="btn-edit"><i class="fa-solid fa-pen-to-square"></i> Edit
+                        </button>
+                        <button class="btn btn-danger btn-mini"><i class="fa-solid fa-trash"></i> Delete</button>
+                        </td>
+                        </tr>`;
 
         $("#tblCustomer").append(all);
 
