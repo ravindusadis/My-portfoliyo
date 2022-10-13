32a249b2eb41d2fb2e304edb6a2beda5cc9ec03b
ravindusadis <ravindusadis01@gmail.com>
2022-10-13 09:54:38 -0700

    load all customer

--- assignment/JS/SPA/controller/CustomerController.js
+++ assignment/JS/SPA/controller/CustomerController.js
@@ -12,6 +12,9 @@ $("#saveCustomer").click(function (){
     customers.push(customer);
 
 
+    loadAllCustomers()
+
+
 
 
 });
@@ -22,4 +25,18 @@ var customerObject = {
     address : customerAddress,
     contact : customerContact,
     salary : customerSalary
+}
+function loadAllCustomers() {
+    $("#tblCustomer").empty();
+    for (var customer of customers) {
+
+        var all = `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.address}</td><td>${customer.contact}</td><td>${customer.salary}</td><td class="odd" style="width: 190px">
+                            <button class="btn btn-warning btn-mini" data-bs-target="#editCustomers"
+                                    data-bs-toggle="modal"><i class="fa-solid fa-pen-to-square"></i> Edit
+                            </button>
+                            <button class="btn btn-danger btn-mini"><i class="fa-solid fa-trash"></i> Delete</button>
+                        </td></tr>`;
+
+        $("#tblCustomer").append(all);
+
 }
